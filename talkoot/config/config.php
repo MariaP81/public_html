d6e1c3cf491c337ef92ccbb0be9e4a1f262d2fd6
BE21 - Maria Pentikäinen ja Leena Ojala <maria.pentikainen@edu.sasky.fi>
2021-12-14 19:14:34 +0200

    hakemistoviittaukset vakioiksi

--- talkoot/config/config.php
+++ talkoot/config/config.php
@@ -5,5 +5,11 @@
       "baseUrl" => "/~be21_mp_lo/talkoot"
     )
   );
+  
+  define("PROJECT_ROOT", dirname(__DIR__) . "/");
+  define("HELPERS_DIR", PROJECT_ROOT . "src/helpers/");
+  define("TEMPLATE_DIR", PROJECT_ROOT . "src/view/");
+  define("MODEL_DIR", PROJECT_ROOT . "src/model/");
+  define("CONTROLLER_DIR", PROJECT_ROOT . "src/controller/");
 
 ?>
