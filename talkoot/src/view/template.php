739bc951ac2909c0e390a28c615d1ae0cec5770b
BE21 - Maria Pentikäinen ja Leena Ojala <maria.pentikainen@edu.sasky.fi>
2022-01-04 18:30:09 +0200

    sivuston tyylimääritykset

--- talkoot/src/view/template.php
+++ talkoot/src/view/template.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="fi">
   <head>
-    <title>talkoot - <?=$this->e($title)?></title>
-    <meta charset="UTF-8">    
+  <link href="styles/styles.css" rel="stylesheet">
+  <meta charset="UTF-8">    
   </head>
   <body>
   <header>
-      <h1><a href="<?=BASEURL?>">talkoot</a></h1>
+      <h1><a href="<?=BASEURL?>">Talkoot</a></h1>
     </header>
     <section>
       <?=$this->section('content')?>
     </section>
     <footer>
       <hr>
-      <div>talkoot by Kurpitsa</div>
+      <div>Talkoot by Kurpitsa</div>
     </footer>
   </body>
 </html>
