c70cc255c11f4e5712f3f59e2ed7f58267b2d3ef
BE21 - Maria Pentikäinen ja Leena Ojala <maria.pentikainen@edu.sasky.fi>
2022-01-10 17:22:25 +0200

    if-else-rakenne korvattu switch-rakenteella

--- talkoot/public/index.php
+++ talkoot/public/index.php
@@ -15,26 +15,30 @@
 
   // Selvitetään mitä sivua on kutsuttu ja suoritetaan sivua vastaava
   // käsittelijä.
-  if ($request === '/' || $request === '/tapahtumat') {
-    require_once MODEL_DIR . 'tapahtuma.php';
-    $tapahtumat = haeTapahtumat();
-    echo $templates->render('tapahtumat',['tapahtumat' => $tapahtumat]);
-    // ... ehtolauseen alku säilyy sellaisenaan
-  } else if ($request === '/tapahtuma') {
-    require_once MODEL_DIR . 'tapahtuma.php';
-    $tapahtuma = haeTapahtuma($_GET['id']);
-    if ($tapahtuma) {
-      echo $templates->render('tapahtuma',['tapahtuma' => $tapahtuma]);
-    } else {
-      echo $templates->render('tapahtumanotfound');
-    }
-    // ... ehtolauseen alku säilyy sellaisenaan
-  } else if ($request === '/lisaa_tili') {
-    echo $templates->render('lisaa_tili');
-     // ... loput ehtolauseesta säilyy sellaisenaan
-  } else { // ... loput ehtolauseesta säilyy sellaisenaan
-    echo $templates->render('notfound');
-  }
+    switch ($request) {
+      case '/':
+      case '/tapahtumat':
+        require_once MODEL_DIR . 'tapahtuma.php';
+        $tapahtumat = haeTapahtumat();
+        echo $templates->render('tapahtumat',['tapahtumat' => $tapahtumat]);
+        break;
+      case '/tapahtuma':
+        require_once MODEL_DIR . 'tapahtuma.php';
+        $tapahtuma = haeTapahtuma($_GET['id']);
+        if ($tapahtuma) {
+          echo $templates->render('tapahtuma',['tapahtuma' => $tapahtuma]);
+        } else {
+          echo $templates->render('tapahtumanotfound');
+        }
+        break;
+      case '/lisaa_tili':
+        echo $templates->render('lisaa_tili');
+        break;
+      default:
+        echo $templates->render('notfound');
+    } 
+       
+     
 
 ?> 
 
