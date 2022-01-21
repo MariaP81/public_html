b0215af712dde53a984936f6a77959b4184b62b5
BE21 - Maria Pentikäinen ja Leena Ojala <maria.pentikainen@edu.sasky.fi>
2022-01-21 18:01:03 +0200

    uloskirjautuminen

--- talkoot/public/index.php
+++ talkoot/public/index.php
@@ -71,7 +71,12 @@
           }
           break;
     
-    
+          case "/logout":
+            require_once CONTROLLER_DIR . 'kirjaudu.php';
+            logout();
+            header("Location: " . $config['urls']['baseUrl']);
+            break;
+      
 
     default:
       echo $templates->render('notfound');
