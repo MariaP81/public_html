caf57735845174ca2e3c3fb859806a93db67b234
BE21 - Maria Pentikäinen ja Leena Ojala <maria.pentikainen@edu.sasky.fi>
2022-01-21 18:16:36 +0200

    ilmoittautumistietojen välitys sivupohjalle

--- talkoot/public/index.php
+++ talkoot/public/index.php
@@ -34,15 +34,24 @@
       $tapahtumat = haeTapahtumat();
       echo $templates->render('tapahtumat',['tapahtumat' => $tapahtumat]);
       break;
-    case '/tapahtuma':
-      require_once MODEL_DIR . 'tapahtuma.php';
-      $tapahtuma = haeTapahtuma($_GET['id']);
-      if ($tapahtuma) {
-        echo $templates->render('tapahtuma',['tapahtuma' => $tapahtuma]);
-      } else {
-        echo $templates->render('tapahtumanotfound');
-      }
-      break;
+      case '/tapahtuma':
+        require_once MODEL_DIR . 'tapahtuma.php';
+        require_once MODEL_DIR . 'ilmoittautuminen.php';
+        $tapahtuma = haeTapahtuma($_GET['id']);
+        if ($tapahtuma) {
+          if ($loggeduser) {
+            $ilmoittautuminen = haeIlmoittautuminen($loggeduser['idhenkilo'],$tapahtuma['idtapahtuma']);
+          } else {
+            $ilmoittautuminen = NULL;
+          }
+          echo $templates->render('tapahtuma',['tapahtuma' => $tapahtuma,
+                                               'ilmoittautuminen' => $ilmoittautuminen,
+                                               'loggeduser' => $loggeduser]);
+        } else {
+          echo $templates->render('tapahtumanotfound');
+        }
+        break;
+  
       // ... switch-lauseen alku säilyy sellaisenaan
       case '/lisaa_tili':
         if (isset($_POST['laheta'])) {
