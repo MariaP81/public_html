864ebada329affca0ca2f7a0b9b1c3c9ef21db1c
BE21 - Maria Pentikäinen ja Leena Ojala <maria.pentikainen@edu.sasky.fi>
2022-01-26 14:38:41 +0200

    ilmoittaudu-nappi tapahtumasivulle

--- talkoot/public/index.php
+++ talkoot/public/index.php
@@ -3,6 +3,9 @@
   // Aloitetaan istunnot.
   session_start();
 
+    // Suoritetaan projektin alustusskripti.
+    require_once '../src/init.php';
+
   // Haetaan kirjautuneen käyttäjän tiedot.
   if (isset($_SESSION['user'])) {
     require_once MODEL_DIR . 'henkilo.php';
@@ -12,8 +15,7 @@
   }
 
 
-  // Suoritetaan projektin alustusskripti.
-  require_once '../src/init.php';
+
 
   // Siistitään polku urlin alusta ja mahdolliset parametrit urlin lopusta.
   // Siistimisen jälkeen osoite /~koodaaja/lanify/tapahtuma?id=1 on 
